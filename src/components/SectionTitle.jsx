0cc56cb229246ea5b6bccb778901f52266d083d2
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-11-21 02:20:52 +0400

    Checkout Form

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 function SectionTitle({ text }) {
   return (
-    <div className="border-b border-base-300 pb-5">
+    <div className="border-b border-base-300 pb-5 align-element">
       <h2 className="text-3xl font-medium tracking-wider capitalize">{text}</h2>
     </div>
   );
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -19,13 +19,13 @@ const Checkout = () => {
     return <SectionTitle text="Your cart is empty" />;
   }
   return (
-    <>
-      <SectionTitle text="place your order" />
+    <div className="align-element">
+      <SectionTitle text="place your order " />
       <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
         <CheckoutForm />
         <CartTotals />
       </div>
-    </>
+    </div>
   );
 };
 export default Checkout;
